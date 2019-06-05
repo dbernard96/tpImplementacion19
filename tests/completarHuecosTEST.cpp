d245405135c74d229f3b25e35d93d469dbe648af
dbernard96 <davidbernard456@gmail.com>
2019-06-05 05:29:32 -0300

    agregue mas test a completarHuecosTEST

--- tests/completarHuecosTEST.cpp
+++ tests/completarHuecosTEST.cpp
@@ -12,7 +12,7 @@ TEST(completarHuecosTEST, noHayHuecos){
     EXPECT_EQ(v,v0);
 }
 
-TEST(completarHuecosTEST,unSoloHueco){
+TEST(completarHuecosTEST,unSoloHuecoOrd){
     viaje v = {make_tuple(0,make_tuple(1,2)),make_tuple(1,make_tuple(-1000,-1000)),make_tuple(2,make_tuple(3,2))};
     vector<int> faltantes = {1};
     completarHuecos(v,faltantes);
@@ -20,7 +20,7 @@ TEST(completarHuecosTEST,unSoloHueco){
     EXPECT_EQ(distMts(get<1>(v[1]),get<1>(v[0])), calculoDistHueco(v[0],v[2],v[1]));
 }
 
-TEST(completarHuecosTEST, multiplesHuecosSeparados){
+TEST(completarHuecosTEST, multiplesHuecosSeparadosOrd){
     viaje v = {make_tuple(0,make_tuple(1,2)),make_tuple(1,make_tuple(-1000,-1000)),make_tuple(2,make_tuple(3,2)),
             make_tuple(3,make_tuple(3,3)),make_tuple(4,make_tuple(-1000,-1000)),make_tuple(5,make_tuple(4,3))};
     vector<int> faltantes = {1,4};
@@ -32,7 +32,7 @@ TEST(completarHuecosTEST, multiplesHuecosSeparados){
     }
 }
 
-TEST(completarHuecosTEST,multiplesHuecosJuntos){
+TEST(completarHuecosTEST,multiplesHuecosJuntosOrd){
     viaje v = {make_tuple(0,make_tuple(1,2)),make_tuple(1,make_tuple(-1000,-1000)),
                make_tuple(4,make_tuple(-1000,-1000)),make_tuple(5,make_tuple(4,3))};
     vector<int> faltantes = {1,2};
@@ -41,5 +41,40 @@ TEST(completarHuecosTEST,multiplesHuecosJuntos){
     for (int i = 0; i < faltantes.size(); ++i) {
         distancia res = distMts(obtenerPosicion(v[faltantes[i]]),obtenerPosicion(v[0]));
         EXPECT_EQ(res,calculoDistHueco(v[0],v[v.size()-1],v[faltantes[i]]));
+    }
+}
+
+TEST(completarHuecosTEST,unSoloHuecoNoOrd){
+    viaje v = {make_tuple(2,make_tuple(3,2)),make_tuple(1,make_tuple(-1000,-1000)),make_tuple(0,make_tuple(1,2))};
+    vector<int> faltantes = {1};
+    completarHuecos(v,faltantes);
+
+    EXPECT_EQ(distMts(get<1>(v[1]),get<1>(v[2])), calculoDistHueco(v[2],v[0],v[1]));
+}
+
+TEST(completarHuecosTEST, multiplesHuecosSeparadosNoOrd){
+    viaje v = {make_tuple(5,make_tuple(4,3)),make_tuple(1,make_tuple(-1000,-1000)),make_tuple(2,make_tuple(3,2)),
+               make_tuple(0,make_tuple(1,2)),make_tuple(4,make_tuple(-1000,-1000)),make_tuple(3,make_tuple(3,3))};
+    vector<int> faltantes = {1,4};
+
+    completarHuecos(v,faltantes);
+
+    distancia res = distMts(obtenerPosicion(v[faltantes[0]]),obtenerPosicion(v[3]));
+    EXPECT_EQ(res,calculoDistHueco(v[3],v[2],v[faltantes[0]]));
+
+    res = distMts(obtenerPosicion(v[faltantes[1]]),obtenerPosicion(v[5]));
+    EXPECT_EQ(res,calculoDistHueco(v[5],v[0],v[faltantes[1]]));
+
+}
+
+TEST(completarHuecosTEST,multiplesHuecosJuntosNoOrd){
+    viaje v = {make_tuple(5,make_tuple(4,3)),make_tuple(1,make_tuple(-1000,-1000)),
+               make_tuple(4,make_tuple(-1000,-1000)),make_tuple(0,make_tuple(1,2))};
+    vector<int> faltantes = {1,2};
+    completarHuecos(v,faltantes);
+
+    for (int i = 0; i < faltantes.size(); ++i) {
+        distancia res = distMts(obtenerPosicion(v[faltantes[i]]),obtenerPosicion(v[v.size()-1]));
+        EXPECT_EQ(res,calculoDistHueco(v[v.size()-1],v[0],v[faltantes[i]]));
     }
 }
